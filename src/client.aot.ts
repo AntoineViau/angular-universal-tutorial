940793ce12178486efc6bbf74714f7d41f308538
Antoine Viau <antoine.viau@gmail.com>
2016-11-17 15:26:46 +0100

    Similarisation de client.aot.ts et client.ts

--- src/client.aot.ts
+++ src/client.aot.ts
@@ -1,6 +1,12 @@
 import 'angular2-universal-polyfills';
-import { platformBrowser }    from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
+import { platformUniversalDynamic } from 'angular2-universal';
 import { BrowserMainModuleNgFactory } from '../aot/src/app/app.browser.module.ngfactory';
 
-platformBrowser().bootstrapModuleFactory(BrowserMainModuleNgFactory);
- 
+enableProdMode();
+
+const platformRef = platformUniversalDynamic();
+document.addEventListener('DOMContentLoaded', () => {
+    platformRef.bootstrapModuleFactory(BrowserMainModuleNgFactory);
+});
+
--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,9 @@
 import 'angular2-universal-polyfills';
 import { enableProdMode } from '@angular/core';
 import { platformUniversalDynamic } from 'angular2-universal';
+import { BrowserMainModule } from './app/app.browser.module';
 
 enableProdMode();
-
-import { BrowserMainModule } from './app/app.browser.module';
 
 const platformRef = platformUniversalDynamic();
 document.addEventListener('DOMContentLoaded', () => {
